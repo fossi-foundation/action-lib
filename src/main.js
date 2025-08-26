e36efe0a4854adc608939a2bf4de2ff5f81b2ced
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2025-08-26 08:19:55 +0200

    Add extra_conf to nix

--- src/main.js
+++ src/main.js
@@ -29,10 +29,10 @@ export async function devbox_check_pkg_version(pkgName, version) {
     return resolvedVersion;
 }
 
-export async function nix_ensure() {
+export async function nix_ensure(extra_conf) {
     core.startGroup("Ensuring Nix is installed");
     if (!fs.existsSync(`/nix/var/nix/profiles/default/bin/nix`)) {
-        let installerDir = tc.find('nix-installer', '3.8.6', process.arch);
+        const installerDir = tc.find('nix-installer', '3.8.6', process.arch);
         if (!installerDir) {
             core.info("Nix installer not cached. Downloading");
             let arch = process.arch === "arm64" ? "aarch64" : "x86_64";
@@ -40,8 +40,10 @@ export async function nix_ensure() {
             await exec.exec(`chmod +x ${installerFile}`);
             installerDir = await tc.cacheFile(installerFile, 'nix-installer', 'nix-installer', '3.8.6', process.arch);
         }
-        await exec.exec(`${installerDir}/nix-installer install --no-confirm`);
+        const extra_conf_arg = extra_conf ? `--extra-conf "${extra_conf}"` : '';
+        await exec.exec(`${installerDir}/nix-installer install --no-confirm ${extra_conf_arg}`);
     }
+    await core.addPath(`/nix/var/nix/profiles/default/bin`);
     core.endGroup();
 }
 
